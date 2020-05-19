fa02081675f597c64e69cc619acda82ecdaee03c
marwan <marwankefah@gmail.com>
2020-05-19 13:27:19 +0200

    added new collection for inverted index and management functions for indexer

--- src/main/java/com/apt/DBManager.java
+++ src/main/java/com/apt/DBManager.java
@@ -1,11 +1,21 @@
 package com.apt;
 
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
-import org.bson.types.ObjectId;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Indexes;
+import com.mongodb.client.model.IndexOptions;
+
+import com.mongodb.client.model.UpdateOptions;
+import com.mongodb.client.model.Updates;
+
 
 public class DBManager {
 
@@ -13,6 +23,7 @@ public class DBManager {
     private MongoClient mongoClient;
     private MongoDatabase database;
     private MongoCollection<Document> collection;
+    private MongoCollection<Document> invertedCollection;
 
     private static DBManager instance;
 
@@ -21,6 +32,9 @@ public class DBManager {
         this.mongoClient = MongoClients.create();
         this.database = mongoClient.getDatabase("dex");
         this.collection = database.getCollection("processed-pages");
+        this.invertedCollection = database.getCollection("inverted-index");
+        this.invertedCollection.createIndex(Indexes.ascending("token"),new IndexOptions().unique(true));
+        this.invertedCollection.createIndex(Indexes.ascending("Documents._id"),new IndexOptions().unique(true));
     }
 
     public void addProcessedPage(Page page){
@@ -33,8 +47,42 @@ public class DBManager {
 //        processedPage.append("pageLink", page.getOrigin());
         this.collection.insertOne(processedPage);
     }
+    
+    public void updateInvertedWordIndex(String word,ObjectId doc_id,int tf,int idf,int tf_idf)
+    {
+    	  
+    	if(this.collection.find(Filters.eq("_id",doc_id)).limit(1)!= null)
+    	{
+    		UpdateOptions x=new UpdateOptions();
+    		Document subDoc= new Document("_id",doc_id);
+    		subDoc.append("tf",tf);
+    		subDoc.append("idf", idf);
+    		subDoc.append("tf_idf",tf_idf);
+    		this.invertedCollection.updateOne(Filters.eq("token",word), Updates.push("Documents", subDoc)
+    				,new UpdateOptions().upsert(true));	
+    	}
+    	else
+    	{
+    		System.out.println("Error in DB Manager Funtion UpdateInvertedWordindex"
+    				+" invalid doc_id");
+    	}
 
-
+    	
+    }
+    
+    public MongoCursor<Document> getCrawledDocuments()
+    {	
+    	return this.collection.find(Filters.exists("_id")).iterator();
+    	
+    }
+    
+    public long getDocumentsCount()
+    {
+    	
+    	return this.collection.estimatedDocumentCount();
+    	
+    }
+    
     public static DBManager getInstance() {
         if(instance == null) {
             instance = new DBManager();
