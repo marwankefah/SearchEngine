fa30e6af1318ca68436c0b9c0e4b6f05defa0a0c
marwan <marwankefah@gmail.com>
2020-05-30 16:56:55 +0200

    fixed stemming stop words bug

--- src/main/java/com/apt/Indexer.java
+++ src/main/java/com/apt/Indexer.java
@@ -234,14 +234,15 @@ public static void indexDocuments(String[] fieldsToIndex)
     		ListIterator <String> i = list.listIterator();
         	while (i.hasNext()) {
         		String temp=i.next();
-        		if(temp == null) continue;
+        		if(temp == null) {continue;}
+
+        		temp=temp.toLowerCase();
                 if(temp.length()<2 || hashStopWords.contains(temp))
                 {
                 	i.remove();
                 }
                 else
-                {
-            		temp=temp.toLowerCase();
+				{
             		String tstem=s.stem(temp);
             		i.set(tstem);
                 }
